3fd03c09918bb8507d1755fd57f6fefe48feb582
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2023-11-03 01:48:05 -0300

    Create Javadoc

---
+++ BiblioControl/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"BiblioControl","l":"Biblioteca"},{"p":"BiblioControl","l":"BibliotecaInicial"},{"p":"BiblioControl","l":"Libro"},{"p":"BiblioControl","l":"Main"}];updateSearchResults();
